68d4ffa37550c692f65418ce884aaad1c81637e2
McThor2 <j4m3s247@gmail.com>
2023-02-13 21:28:19 +0000

    Added default package location

---
+++ src/Main/Config.lua
@@ -0,0 +1,127 @@
+
+local Config = {}
+
+local CONFIG_ATTRIBUTE_NAME = "rpm_config"
+local DEFAULT_PACKAGE_LOCATION = "ReplicatedStorage/Packages"
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
+local HttpService = game:GetService("HttpService")
+
+local LOCATION_LOOKUP = {
+    ["ReplicatedStorage"] = ReplicatedStorage,
+    ["ServerStorage"] = ServerStorage
+}
+
+Config._decoded = nil
+
+local function parseLocation(rawLocation: string)
+
+    local tokens = string.split(rawLocation, "/")
+    print(tokens)
+
+    local rootLocation = tokens[1]
+
+    local root = LOCATION_LOOKUP[rootLocation]
+    local location = root
+    for i = 2, #tokens do
+        local name = tokens[i]
+        local node = location:FindFirstChild(tokens[i])
+        if not node then
+            node = Instance.new("Folder")
+            node.Name = name
+            node.Parent = location
+        end
+        location = node
+    end
+
+    return location
+end
+
+function Config:Load()
+    local rawConfig = ServerStorage:GetAttribute(CONFIG_ATTRIBUTE_NAME)
+
+    rawConfig = rawConfig or "{}"
+
+    local success, config = pcall(function()
+        return HttpService:JSONDecode(rawConfig)
+    end)
+
+    if not success then
+        warn("Invalid RPM config detected - ")
+        warn(config)
+        return
+    end
+
+    Config._decoded = config
+
+    return config
+end
+
+function Config:Set(key, value)
+
+    local config = self._decoded
+
+    config[key] = value
+
+    local success, encodedConfig = pcall(function()
+        return HttpService:JSONEncode(config)
+    end)
+
+    if not success then
+        warn("Invalid config key/value - ", key, value)
+        warn(encodedConfig)
+        return
+    end
+
+    print("set", encodedConfig)
+    ServerStorage:SetAttribute(
+        CONFIG_ATTRIBUTE_NAME,
+        encodedConfig
+    )
+end
+
+function Config:Get(key)
+
+    if not self._decoded then
+        warn("Config not loaded")
+        return nil
+    end
+    
+    return self._decoded[key]
+end
+
+function Config:GetPackageLocation()
+
+    local rawLocation = self:Get("PackageLocation")
+
+    print(rawLocation)
+
+    return parseLocation(rawLocation)
+end
+
+local function init()
+    
+    local placeConfig = Config:Load()
+
+	print(placeConfig)
+
+	if not placeConfig or not placeConfig["PackageLocation"] then
+        Config:Set("PackageLocation", DEFAULT_PACKAGE_LOCATION)
+	end
+
+    ServerStorage.AttributeChanged:Connect(function(attribute)
+        if attribute ~= CONFIG_ATTRIBUTE_NAME then
+            return
+        end
+
+        local newConfig = Config:Load()
+
+        print(newConfig)
+    end)
+
+end
+
+init()
+
+return Config
--- src/Main/init.server.lua
+++ src/Main/init.server.lua
@@ -6,32 +6,26 @@ local _version = "0.1.0"
 
 local root = script.Parent
 
-local GitHubApi = require(script:WaitForChild("GitHubApi")) 
+local GitHubApi = require(script:WaitForChild("GitHubApi"))
 local WallyApi = require(script:WaitForChild("WallyApi"))
 local GUI = require(script:WaitForChild("GUI"))
+local Config = require(script:WaitForChild("Config"))
 
 local Selection = game:GetService("Selection")
-local ChangeHistory = game:GetService("ChangeHistoryService")
 local ServerStorage = game:GetService("ServerStorage")
-local StudioService = game:GetService("StudioService")
 
-local textHistory = {}
-
+local RPM_SETTINGS_KEY = "rpm_settings"
 
 local GH_TAG_PATTERN = "^([%w-]+)/([%w-]+)@(%w+.%w+.%w+)$"
 local GH_PATTERN = "^(%a+)/(%a+)$"
 
 local function onDownload(url: string)
-	table.insert(textHistory, url)
 
 	local scope, name, ver = string.match(url, GH_TAG_PATTERN)
 
 	local selected = Selection:Get()
 
 	local parent = ServerStorage.Packages
-	if #selected > 0 then
-		--parent = selected[1]
-	end
 	
 	-- TODO: Search for existing package
 
@@ -64,8 +58,30 @@ end
 local function onWally(rawText: string)
 	local packagesInfo = WallyApi:ListPackages(rawText)
 	GUI:UpdateSearchResults(packagesInfo, onResultRow)
+
 end
 
-GUI:Init(plugin)
-GUI:RegisterDownloadCallback(onDownload)
-GUI:RegisterWallySearch(onWally)
+local function init()
+
+	GUI:Init(plugin)
+	GUI:RegisterDownloadCallback(onDownload)
+	GUI:RegisterWallySearch(onWally)
+
+	GUI.Opened:Connect(function()
+		local packageLocation = Config:GetPackageLocation()
+		print(packageLocation:GetFullName())
+	end)
+
+	local placeSettings = plugin:GetSetting(RPM_SETTINGS_KEY)
+
+	if placeSettings == nil then
+		plugin:SetSetting(RPM_SETTINGS_KEY, {})
+	end
+
+	local packageLocation = Config:GetPackageLocation()
+
+	print(packageLocation:GetFullName())
+
+end
+
+init()
